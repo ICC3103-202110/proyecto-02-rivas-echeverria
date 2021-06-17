c66ed3e86a0d564a1c64ffedf4900403cab22f39
nicolas echeverria <necheverria@miuandes.cl>
2021-06-17 03:32:30 -0400

    apikey3

---
+++ update.js
@@ -0,0 +1,81 @@
+const ApiKey:'8454e5211589480a174e7488a24c7fcf'
+
+function API (){
+
+}
+
+
+
+
+function add (model){
+    const cities2: [],
+    const temp2: ,
+    constmax2: ,
+    const min2: 
+
+    return {
+        ...model,
+        cities: ,
+        temp: ,
+        max: ,
+        min: 
+    }
+}
+
+function del (model) {
+    const cities2: ['no city'],
+    const temp2: ,
+    constmax2: ,
+    const min2: 
+
+    return {
+        ...model,
+        cities: ,
+        temp: ,
+        max: ,
+        min: 
+    }
+}
+
+
+function update (model){
+    const cities2:[] ,
+    const temp2: ,
+    constmax2: ,
+    const min2: 
+
+    return {
+        ...model,
+        cities: ,
+        temp: ,
+        max: ,
+        min: 
+    }
+}
+
+
+module.exports = {
+    update,
+    add,
+    del
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
