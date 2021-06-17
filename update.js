3d64b6371e2022b8f83b1de547aa7557f16211c3
CarlosRivas <crivas1@miuandes.cl>
2021-06-17 12:11:15 -0300

    add, update y delete funcionando

--- update.js
+++ update.js
@@ -6,12 +6,11 @@ const axios= require("axios")
 
 
 async function API (city){
-    const url=`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8454e5211589480a174e7488a24c7fcf`
+    const url=`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8454e5211589480a174e7488a24c7fcf&units=metric`
     const weather = await axios.get(url)
 
     return [city,weather.data.main.temp,weather.data.main.temp_max,weather.data.main.temp_min]
 }
-//Amsterdam
 
 async function ModelUpdate(model,option){
     
@@ -41,9 +40,7 @@ async function ModelUpdate(model,option){
         const max2= data[2]
         const min2= data[3]
         for (var l=0; l < Object.keys(model).length; l++){
-            console.log('entra a for')
             if (model[l].cities==cities2){
-                console.log('entra a if')
                 model[l].cities = cities2
                 model[l].temp = temp2
                 model[l].max = max2
@@ -61,27 +58,18 @@ async function ModelUpdate(model,option){
         
     
     if (option=='Delete City'){
-        const CN=await SelectCity()
-        const cities2= CN
+        const CN=await SelectCity(model)
+        const cities2= CN['selectCity']
+        const newModel = []
         for (var l=0; l < Object.keys(model).length; l++){
-            if (model[l].cities==cities2){
-                const a=model[l].cities.splice(l,1)
-                const b=model[l].temp.splice(l,1)
-                const c=model[l].max.splice(l,1)
-                const d=model[l].min.splice(l,1)
-                return{
-                    ...model[l],
-                    cities:a ,
-                    temp: b,
-                    max: c,
-                    min:d 
+            if (model[l].cities!==cities2){
+                newModel.push(model[l])
 
                 }
             }
+        return newModel
         } 
     
-
-    }
     
     else{
         console.log(option," :option")
